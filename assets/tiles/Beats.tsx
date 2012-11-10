f148e6154e2354cfaa2f304d8fac7d293e7c6864
PureChance <rtmcassidy@gmail.com>
2012-11-10 18:35:57 +0000

    Tileset created.

---
+++ assets/tiles/Beats.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Beats" tilewidth="64" tileheight="64">
+ <image source="../Beats.png" width="320" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="Beat" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Beat" value="2"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Beat" value="3"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Beat" value="4"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Beat" value="5"/>
+  </properties>
+ </tile>
+</tileset>
