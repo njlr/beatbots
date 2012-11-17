bf49b370d5cc372f414abef297f1ffc4ff64d500
Richard <rtmcassidy@gmail.com>
2012-11-17 15:53:56 +0000

    Added song map.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Circuit Components
 Generator - Successor
 Node - Successor
 Speaker
-Colorizer - Color { 0 - Red, 1 - Blue, 2 - Yellow, 3 - Magenta, 4 - Orange, 5 - Green }
+Colorizer - Color { 0 - Red, 1 - Blue, 2 - Yellow, 3 - Magenta, 4 - Orange, 5 - Green, 6 - White }
 Switch - SuccessorA, SuccessorB
 Alternator
 
--- assets/tiles/Beats.tsx
+++ assets/tiles/Beats.tsx
@@ -3,37 +3,37 @@
  <image source="../Beats.png" width="256" height="32"/>
  <tile id="0">
   <properties>
-   <property name="Beat" value="1"/>
+   <property name="Color" value="0"/>
   </properties>
  </tile>
  <tile id="1">
   <properties>
-   <property name="Beat" value="2"/>
+   <property name="Color" value="5"/>
   </properties>
  </tile>
  <tile id="2">
   <properties>
-   <property name="Beat" value="3"/>
+   <property name="Color" value="1"/>
   </properties>
  </tile>
  <tile id="3">
   <properties>
-   <property name="Beat" value="4"/>
+   <property name="Color" value="2"/>
   </properties>
  </tile>
  <tile id="4">
   <properties>
-   <property name="Beat" value="4"/>
+   <property name="Color" value="3"/>
   </properties>
  </tile>
  <tile id="5">
   <properties>
-   <property name="Beat" value="5"/>
+   <property name="Color" value="4"/>
   </properties>
  </tile>
  <tile id="6">
   <properties>
-   <property name="Beat" value="0"/>
+   <property name="Color" value="6"/>
   </properties>
  </tile>
 </tileset>
